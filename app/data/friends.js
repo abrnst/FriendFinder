9ba19366d115f37abf1d6a53823506250d74aedc
Andrew Berndt <abrnst@gmail.com>
2018-07-06 21:51:24 -0500

    almost done

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+
+
+var friendsArray = [
+    {
+    "name":"Wall-E",
+    "profilePic":"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjZ_cyN_YvcAhUKKKwKHb1bAk4QjRx6BAgBEAU&url=http%3A%2F%2Fbattlefordreamisland.wikia.com%2Fwiki%2FFile%3AWall-e.jpg&psig=AOvVaw309I-8IQxh3VpgTUkjmDZM&ust=1531018121077043",
+    "scores":[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+  },
+  {
+    "name":"Fry ",
+    "profilePic":"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj9wvfx_IvcAhVCYK0KHdjWA1cQjRx6BAgBEAU&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FPhilip_J._Fry&psig=AOvVaw1sI0-DRrBd-ZO_k44XmAOr&ust=1531018066036736",
+    "scores":[
+        "3",
+        "1",
+        "4",
+        "2",
+        "5",
+        "3",
+        "2",
+        "5",
+        "2",
+        "1"
+      ]
+  },
+  {
+    "name":"Bart Simpson",
+    "profilePic":"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjw9rrV_IvcAhVDRK0KHaKGDKMQjRx6BAgBEAU&url=http%3A%2F%2Fsimpsons.wikia.com%2Fwiki%2FBart_Simpson&psig=AOvVaw0zZVeA__vFvbwcxYFOg8kA&ust=1531018006290186",
+    "scores":[
+        "1",
+        "2",
+        "4",
+        "2",
+        "2",
+        "4",
+        "2",
+        "5",
+        "5",
+        "1"
+      ]
+  }
+]
+
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,43 @@
+
+
+var friends = require("../data/friends")
+
+
+module.exports = function(app){
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res) {
+        
+       
+        var newFriendScores = req.body.scores;
+        var scoreArray = [];
+        var friendCount = 0;
+        var bestMatch = 0;
+        
+
+        for (var i; i <friends.length; i++) {
+            var scoreDiff = 0;
+            for (var j=0; j < newFriendScores.length; j++) {
+                scoreDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newFriendScores[j])));
+
+            }
+
+            scoreArray.push(scoreDiff);
+        }
+
+        for(var i=0; i<scoreArray.length; i++){
+            if(scoreArray[i] <= scoreArray[bestMatch]){
+              bestMatch = i;
+            }
+          }
+
+        
+
+          var bestFriend = friends[bestMatch];
+          res.json(bestFriend);
+
+        friends.push(req.body);
+    });
+};
